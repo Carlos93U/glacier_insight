d8ea4c950aa8c1ff489b73d70b22df9444e8b37e
Charly <huillcas.juan3@gmail.com>
2024-11-03 12:26:46 -0500

    app file modify

--- app.py
+++ app.py
@@ -3,31 +3,37 @@ from PIL import Image
 import requests
 from io import BytesIO
 
-# Título de la aplicación
-st.title("Comparación de Glaciares en Cordilleras")
+# Title of the application
+st.title("Glacier Comparison in Mountain Ranges")
 
-# Lista desplegable para seleccionar la cordillera
-option = st.selectbox("Seleccione la cordillera:", ["Blanca", "Huallanca"])
+# Dictionary to map mountain range names to their IDs
+mountain_ranges = {
+    "Blanca": "1", "Huallanca": "2", "Huayhuash": "3", "Raura": "4", "Huagoruncho": "5",
+    "La Viuda": "6", "Central": "7", "Huaytapallana": "8", "Chonta": "9", "Ampato": "10",
+    "Vilcabamba": "11", "Urubamba": "12", "Huanzo": "13", "Chila": "14", "Raya": "15",
+    "Vilcanota": "16", "Carabaya": "17", "Apolobamba": "18", "Volcanica": "19", "Barroso": "20"
+}
 
-# Definición de las rutas de las imágenes según la selección del usuario
+# Dropdown list for selecting the mountain range
+option = st.selectbox("Choose Mountain Range:", list(mountain_ranges.keys()))
+
+# Base URL for the images
 base_url = "https://github.com/Carlos93U/glacier_insight/raw/first-version/data/raw"
-if option == "Blanca":
-    glacier_image_url = f"{base_url}/1_Blanca/Glaciers_comparison.png"
-    histogram_image_url = f"{base_url}/1_Blanca/histograms_comparison.png"
-    title = "Cordillera Blanca 1989 - 2020"
-else:
-    glacier_image_url = f"{base_url}/2_Huallanca/Glaciers_comparison.png"
-    histogram_image_url = f"{base_url}/2_Huallanca/histograms_comparison.png"
-    title = "Cordillera Huallanca 1989 - 2020"
 
-# Mostrar el título para la comparación de glaciares
+# Generate URLs and title dynamically based on selected option
+mountain_id = mountain_ranges[option]
+glacier_image_url = f"{base_url}/{mountain_id}_{option.replace(' ', '_')}/Glaciers_comparison.png"
+histogram_image_url = f"{base_url}/{mountain_id}_{option.replace(' ', '_')}/histograms_comparison.png"
+title = f"Mountain Range {option} 1989 - 2020"
+
+# Display the title for glacier comparison
 st.subheader(title)
 response_glacier = requests.get(glacier_image_url)
 glacier_image = Image.open(BytesIO(response_glacier.content))
-st.image(glacier_image, caption="Comparación de Glaciares")
+st.image(glacier_image, caption="Glacier Comparison")
 
-# Mostrar el título para el histograma
-st.subheader("Histogramas 1989 - 2020")
+# Display the title for histogram
+st.subheader("Histograms 1989 - 2020")
 response_histogram = requests.get(histogram_image_url)
 histogram_image = Image.open(BytesIO(response_histogram.content))
-st.image(histogram_image, caption="Comparación de Histogramas")
+st.image(histogram_image, caption="Histogram Comparison")
